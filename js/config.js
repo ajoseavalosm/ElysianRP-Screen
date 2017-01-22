efa86c9f064601cb1c9beb8656fe24fb8150c070
ajoseavalosm <ajoseavalosm@gmail.com>
2017-01-22 13:54:57 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -97,7 +97,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 2;
+var l_musicVolume = 5;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
